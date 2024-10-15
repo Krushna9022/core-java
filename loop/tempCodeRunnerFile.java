e2e568d341bd8f04193a1453de6ad05c3517af3d
Krushna9022 <158745029+Krushna9022@users.noreply.github.com>
2024-10-16 01:39:53 +0530

    Delete loop/tempCodeRunnerFile.java

--- loop/tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-System.out.println("I.");
-		Thread.sleep(500);
-		System.out.println("I..");
-		Thread.sleep(500);
-		System.out.println("I L.. ");
-		System.out.println("I love..");
-		Thread.sleep(900);
-		System.out.println("I Love YOU !!");
